242f1c66efe62cbcdbeb2865ba4c5e2a59808317
Vipkry <lolpsxvipkry2@gmail.com>
2015-05-15 22:50:24 -0300

    pulo final, sem gravidade

--- Jogo.py
+++ Jogo.py
@@ -26,19 +26,21 @@ personagem = Sprite("Imagens/Jogo/personagem_teste.png")
 chao = Sprite("Imagens/Jogo/chao.png")
 # Posições da corrida
 chao.set_position(0, janela.height - chao.height)
-personagem.set_position(10, janela.height - chao.height - personagem.height)
 # Física
-personagem_speed_Y = 250
+personagem_speed_Y = 0
 personagem_speed_X = 300
 altura_do_pulo = 100
 altura_inicial = 0
 flag_pulo = False
 ja_pulou = False
+
 # Game loop
 while True:
     # Mudando de tela
     if mouse.is_button_pressed(1) and mouse.is_over_object(icone_jogo) and menu == True:
         menu = False
+        # posicionando o personagem aqui pois assim podemos jogar múlltiplas vezes após perder o jogo
+        personagem.set_position(10, janela.height - chao.height - personagem.height)
     # Condição para saber em qual parte do jogo o jogador está (menu ou corrida)
     if menu:
         # RENDER Menu
@@ -51,16 +53,25 @@ while True:
     else:
         # Input
         personagem.move_key_x(personagem_speed_X * janela.delta_time())
-        # Pulo -> Agora tem que fazer o personagem parar quando colidir com o chão
+        # Pulo -> Agora tem que fazer isso virar uma função legal (miguel)
         if teclado.key_pressed("UP"):
             if flag_pulo == False:
                 altura_inicial = personagem.y
+                personagem_speed_Y = 220
                 flag_pulo = True
-        if personagem.y > altura_inicial - altura_do_pulo and flag_pulo == True and ja_pulou ==  False:
+        if personagem.y > altura_inicial - altura_do_pulo and flag_pulo == True and ja_pulou == False:
             personagem.move_y(-personagem_speed_Y * janela.delta_time())
         elif flag_pulo == True:
             personagem.move_y(personagem_speed_Y * janela.delta_time())
             ja_pulou = True
+            if personagem.collided(chao):
+                flag_pulo = False
+                ja_pulou = False
+        # Game over
+        # Acontece quando o jogador cai ou bateem um dos espinhos
+        if personagem.y > janela.height:
+            menu = True
+
         # RENDER Jogo
         fundo_menu.draw()
         logo.draw()
